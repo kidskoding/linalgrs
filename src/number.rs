a46848de20bd8b4c0b09eecde015599853e01781
Anirudh Konidala <kidskodingclub@gmail.com>
2025-02-18 05:05:35 -0600

    Number type no longer accounts for unsigned integers. It only now accounts for signed integers

--- src/number.rs
+++ src/number.rs
@@ -15,11 +15,6 @@ pub trait Number:
     + Default 
     + PartialEq {}
 
-impl Number for u8 {}
-impl Number for u16 {}
-impl Number for u32 {}
-impl Number for u64 {}
-impl Number for u128 {}
 impl Number for i8 {}
 impl Number for i16 {}
 impl Number for i32 {}
