79d40809cd04490aa6d7993c141292d7854a32c4
Anirudh Konidala <kidskodingclub@gmail.com>
2025-03-05 12:23:15 -0600

    unsigned types implement the Number trait

--- src/number.rs
+++ src/number.rs
@@ -19,11 +19,17 @@ pub trait Number:
 {
 }
 
+impl Number for u8 {}
+impl Number for u16 {}
+impl Number for u32 {}
+impl Number for u64 {}
+impl Number for u128 {}
+impl Number for usize {}
 impl Number for i8 {}
 impl Number for i16 {}
 impl Number for i32 {}
 impl Number for i64 {}
 impl Number for i128 {}
+impl Number for isize {}
 impl Number for f32 {}
 impl Number for f64 {}
-
